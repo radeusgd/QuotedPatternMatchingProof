7322b35443bb09142ca96beb81135e0195f81755
Radosław Waśko <wasko.radek@gmail.com>
2020-04-03 18:16:55 +0200

    Added splices, fixed value definition (plain terms) and proved canonical forms.

--- calc.v
+++ calc.v
@@ -31,12 +31,13 @@ Notation "T1 '==>' T2" := (TArr T1 T2) (at level 48).
 Inductive typedterm :=
 | TypedTerm (u: term) (t: type)
 with term :=
-| Nat (n : nat)
-| VAR (x : DeBruijnIndex)
-| Lam (argT: type) (ebody : typedterm)
-| App (e1 e2 : typedterm)
-| Lift (e: typedterm)
-| Box (e : typedterm)
+     | Nat (n : nat)
+     | VAR (x : DeBruijnIndex)
+     | Lam (argT: type) (ebody : typedterm)
+     | App (e1 e2 : typedterm)
+     | Lift (e: typedterm)
+     | Quote (e : typedterm)
+     | Splice (e : typedterm)
 (* | PatternMatch (e : typedterm) (pat : pattrn) (success failure : typedterm) *)
 .
 Notation "u ':' T" := (TypedTerm u T) (at level 49).
@@ -59,39 +60,40 @@ Fixpoint traverse_typedterm (f : nat -> nat -> term) l t :=
   | u : T => TypedTerm (traverse_term f l u) T
   end
 with traverse_term f l u :=
-  match u with
-  | Nat n => Nat n
-  | VAR x => f l x
-  | Lam argT ebody => Lam argT (traverse_typedterm f (1 + l) ebody)
-  | App e1 e2 => App (traverse_typedterm f l e1) (traverse_typedterm f l e2)
-  | Lift e => Lift (traverse_typedterm f l e)
-  | Box e => Box (traverse_typedterm f l e)
-  (* | PatternMatch e pat success failure => *)
-  (*   PatternMatch (traverse_typedterm f l e) pat (traverse_typedterm f (pattern_binders_count pat + l) success) (traverse_typedterm f l failure) *)
-  end.
+       match u with
+       | Nat n => Nat n
+       | VAR x => f l x
+       | Lam argT ebody => Lam argT (traverse_typedterm f (1 + l) ebody)
+       | App e1 e2 => App (traverse_typedterm f l e1) (traverse_typedterm f l e2)
+       | Lift e => Lift (traverse_typedterm f l e)
+       | Quote e => Quote (traverse_typedterm f l e)
+       | Splice e => Splice (traverse_typedterm f l e)
+                     (* | PatternMatch e pat success failure => *)
+                     (*   PatternMatch (traverse_typedterm f l e) pat (traverse_typedterm f (pattern_binders_count pat + l) success) (traverse_typedterm f l failure) *)
+       end.
 
 Compute (Nat 0 : TNat).
 
 Instance Traverse_t_tt : Traverse term typedterm := {
-  traverse := traverse_typedterm
-}.
+                                                     traverse := traverse_typedterm
+                                                   }.
 
 Instance Traverse_t_t : Traverse term term := {
-  traverse := traverse_term
+                                               traverse := traverse_term
                                              }.
 Lemma traverse_term_injective:
   forall f,
-  (forall x1 x2 l, f l x1 = f l x2 -> x1 = x2) ->
-  forall (t1 t2 : term) l,
-  traverse_var f l t1 = traverse_var f l t2 ->
-  t1 = t2
+    (forall x1 x2 l, f l x1 = f l x2 -> x1 = x2) ->
+    forall (t1 t2 : term) l,
+      traverse_var f l t1 = traverse_var f l t2 ->
+      t1 = t2
 
 with traverse_typedterm_injective:
-  forall f,
-  (forall x1 x2 l, f l x1 = f l x2 -> x1 = x2) ->
-  forall (t1 t2 : typedterm) l,
-  traverse_var f l t1 = traverse_var f l t2 ->
-  t1 = t2.
+       forall f,
+         (forall x1 x2 l, f l x1 = f l x2 -> x1 = x2) ->
+         forall (t1 t2 : typedterm) l,
+           traverse_var f l t1 = traverse_var f l t2 ->
+           t1 = t2.
 Proof.
   prove_traverse_var_injective.
   prove_traverse_var_injective.
@@ -110,11 +112,11 @@ Qed.
 
 Lemma traverse_functorial_value_value:
   forall f g (t : term) l,
-  traverse g l (traverse f l t) = traverse (fun l x => traverse g l (f l x)) l t
+    traverse g l (traverse f l t) = traverse (fun l x => traverse g l (f l x)) l t
 
 with traverse_functorial_value_term:
-  forall f g (t : typedterm) l,
-  traverse g l (traverse f l t) = traverse (fun l x => traverse g l (f l x)) l t.
+       forall f g (t : typedterm) l,
+         traverse g l (traverse f l t) = traverse (fun l x => traverse g l (f l x)) l t.
 Proof.
   prove_traverse_functorial.
   prove_traverse_functorial.
@@ -131,17 +133,17 @@ Proof. constructor. prove_traverse_identifies_var. Qed.
 
 Lemma traverse_relative_term_term:
   forall f g p (t : term) m l,
-  (forall l x, f (l + p) x = g l x) ->
-  m = l + p ->
-  traverse f m t =
-  traverse g l t
+    (forall l x, f (l + p) x = g l x) ->
+    m = l + p ->
+    traverse f m t =
+    traverse g l t
 
 with traverse_relative_term_typedterm:
-  forall f g p (t : typedterm) m l,
-  (forall l x, f (l + p) x = g l x) ->
-  m = l + p ->
-  traverse f m t =
-  traverse g l t.
+       forall f g p (t : typedterm) m l,
+         (forall l x, f (l + p) x = g l x) ->
+         m = l + p ->
+         traverse f m t =
+         traverse g l t.
 Proof.
   prove_traverse_relative.
   prove_traverse_relative.
@@ -159,15 +161,15 @@ Qed.
 
 Lemma traverse_term_var_is_identity:
   forall f,
-  (forall l x, f l x = var x) ->
-  forall (t : term) l,
-  traverse f l t = t
+    (forall l x, f l x = var x) ->
+    forall (t : term) l,
+      traverse f l t = t
 
 with traverse_typedterm_var_is_identity:
-  forall f,
-  (forall l x, f l x = var x) ->
-  forall (t : typedterm) l,
-  traverse f l t = t.
+       forall f,
+         (forall l x, f l x = var x) ->
+         forall (t : typedterm) l,
+           traverse f l t = t.
 Proof.
   prove_traverse_var_is_identity.
   prove_traverse_var_is_identity.
@@ -189,8 +191,8 @@ Qed.
 
 Lemma lam_is_sane :
   (subst (Nat 42) 0 (Lam TNat (VAR 0 : TNat) : TNat ==> TNat)) = Lam TNat (VAR 0 : TNat) : TNat ==> TNat
-/\ (subst (Nat 42) 0 (Lam TNat (VAR 1 : TNat) : TNat ==> TNat)) = Lam TNat (Nat 42 : TNat) : TNat ==> TNat
-/\ (subst (Nat 42) 0 (Lam TNat (VAR 2 : TNat) : TNat ==> TNat)) = Lam TNat (VAR 1 : TNat) : TNat ==> TNat.
+                                                                                         /\ (subst (Nat 42) 0 (Lam TNat (VAR 1 : TNat) : TNat ==> TNat)) = Lam TNat (Nat 42 : TNat) : TNat ==> TNat
+                                                                                                                                                                                   /\ (subst (Nat 42) 0 (Lam TNat (VAR 2 : TNat) : TNat ==> TNat)) = Lam TNat (VAR 1 : TNat) : TNat ==> TNat.
   auto.
 Qed.
 
@@ -212,7 +214,7 @@ Definition extendEnv (G : TypEnv) (L : Level) (T : type) : TypEnv := (insert 0 (
 Definition env_has (G : TypEnv) (x : DeBruijnIndex) (L : Level) (T : type) : Prop :=
   lookup x G = Some (L, T).
 
-Reserved Notation "G '⊢(' L ')' t '∈' T" (at level 40, t at level 59).
+Reserved Notation "G '⊢(' L ')' t '∈' T" (at level 40, t at level 59, T at level 59).
 Inductive typing_judgement : TypEnv -> Level -> typedterm -> type -> Prop :=
 | T_Nat : forall L G n, G ⊢(L) (Nat n : TNat) ∈ TNat
 | T_Var : forall L G x T,
@@ -225,7 +227,17 @@ Inductive typing_judgement : TypEnv -> Level -> typedterm -> type -> Prop :=
     G ⊢(L) t1 ∈ (T1 ==> T2) ->
     G ⊢(L) t2 ∈ T1 ->
     G ⊢(L) (App t1 t2: T2) ∈ T2
-| T_Lift : forall G t, G ⊢(L0) t ∈ TNat -> G ⊢(L0) (Lift t : ⧈TNat) ∈ ⧈TNat
+| T_Lift : forall G t,
+    G ⊢(L0) t ∈ TNat ->
+    G ⊢(L0) (Lift t : ⧈TNat) ∈ ⧈TNat
+| T_Box : forall G t T,
+    G ⊢(L1) t ∈ T ->
+    G ⊢(L0) (Quote t : ⧈T) ∈ ⧈T
+| T_Unbox : forall G t T,
+    G ⊢(L0) t ∈ ⧈T ->
+    G ⊢(L1) (Splice t : T) ∈ T
+(* | T_Fix : TODO *)
+(* | T_Pat : TODO *)
 where "G '⊢(' L ')' t '∈' T" := (typing_judgement G L t T).
 Hint Constructors typing_judgement : typing_judgement.
 
@@ -234,28 +246,80 @@ Reserved Notation "t1 '-->(' L ')' t2" (at level 48).
 Inductive reducts : Level -> typedterm -> typedterm -> Prop :=
 | E_App1 : forall L t1 t2 t1' T, t1 -->(L) t1' -> (App t1 t2 : T) -->(L) (App t1' t2 : T)
 | E_App2 : forall L t1 t2 t2' T, t2 -->(L) t2' -> (App t1 t2 : T) -->(L) (App t1 t2' : T)
-(* | E_Abs : forall t t' T1 T, *)
-(*   t -->(L1) t' -> *)
-(*   (Lam T1 t : T) -->(L1) (Lam T1 t' : T) *)
+| E_Abs : forall t t' T1 T,
+  t -->(L1) t' ->
+  (Lam T1 t : T) -->(L1) (Lam T1 t' : T)
+| E_Box : forall t t' T,
+    t -->(L1) t' ->
+    (Quote t : T) -->(L0) (Quote t' : T)
+| E_Unbox : forall t t' T,
+    t -->(L0) t' ->
+    (Splice t : T) -->(L1) (Splice t' : T)
 | E_Lift_Red : forall n TN T,
-    (Lift (Nat n : TN) : T) -->(L0) (Box (Nat n : TN) : T)
+    (Lift (Nat n : TN) : T) -->(L0) (Quote (Nat n : TN) : T)
 | E_Lift : forall t t' T,
     t -->(L0) t' ->
     (Lift t : T) -->(L0) (Lift t' : T)
 | E_Beta : forall t T1 T2 v,
     (App (Lam T1 t : (T1 ==> T2)) v : T2) -->(L0) substitute v t
+(* | E_Fix : TODO *)
+(* | E_Fix_Red : TODO *)
+(* | E_Pat : TODO *)
+(* | E_Pat_Succ : TODO *)
+(* | E_Pat_Fail : TODO *)
 where "t1 '-->(' L ')' t2" := (reducts L t1 t2).
 Hint Constructors reducts : reducts.
 
 (* PROPERTIES *)
+Inductive isplain : typedterm -> Prop :=
+| Plain_tt : forall t T, isplaint t -> isplain (t : T)
+with
+isplaint : term -> Prop :=
+| Plain_nat : forall n, isplaint (Nat n)
+| Plain_var : forall x, isplaint (VAR x)
+| Plain_lam : forall T t, isplain t -> isplaint (Lam T t)
+| Plain_app : forall t1 t2, isplain t1 -> isplain t2 -> isplaint (App t1 t2)
+(* | Plain_fix : TODO *)
+.
 
 Inductive isvalue : typedterm -> Prop :=
 | Val_Nat : forall n T, isvalue (Nat n : T)
 | Val_Lam : forall t T1 T, isvalue (Lam T1 t : T)
-| Val_Box : forall t T, isvalue (Box t : T)
+| Val_Box : forall t T, isplain t -> isvalue (Quote t : T)
 .
 
 Definition id_nat := (Lam TNat (VAR 0 : TNat) : TNat ==> TNat).
 Lemma test_red : (App id_nat (Nat 42 : TNat) : TNat) -->(L0) (Nat 42 : TNat).
   apply E_Beta.
+Qed.
+
+Lemma CanonicalForms1 : forall G t,
+    G ⊢(L0) t ∈ TNat ->
+    isvalue t ->
+    exists n, t = (Nat n : TNat).
+  intros.
+  inversion H0; inversion H; subst; try congruence.
+  inversion H.
+  exists n. trivial.
+Qed.
+
+Lemma CanonicalForms2 : forall G t T1 T2,
+    G ⊢(L0) t ∈ (T1 ==> T2) ->
+    isvalue t ->
+    exists t', t = (Lam T1 t' : T1 ==> T2).
+  intros.
+  inversion H0; inversion H; subst; try congruence.
+  inversion H5. subst. eexists. auto.
+Qed.
+
+Lemma CanonicalForms3 : forall G t T,
+    G ⊢(L0) t ∈ (⧈T) ->
+    isvalue t ->
+    exists t', isplain t' /\ t = (Quote t' : ⧈T).
+  intros.
+  inversion H0; inversion H; subst; try congruence.
+  eexists.
+  constructor.
+  exact H1.
+  congruence.
 Qed.
